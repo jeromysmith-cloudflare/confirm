ba80057516c625dd0543ca8c53dcc359d27a7d26
jeromysmith-cloudflare <jeromysmith@gmail.com>
2026-02-04 16:16:29 -0600

    Update resources.js

--- functions/api/resources.js
+++ functions/api/resources.js
@@ -1,5 +1,10 @@
-export async function onRequest() {
-  return new Response("FUNCTION HIT OK", {
-    headers: { "Cache-Control": "no-store" }
+export async function onRequest({ env }) {
+  const data = await env.RESOURCES_KV.get("resources");
+
+  return new Response(data ?? "[]", {
+    headers: {
+      "Content-Type": "application/json",
+      "Cache-Control": "no-store" // keep this while testing
+    }
   });
 }
