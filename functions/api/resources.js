2343b106b62c99f23e1eed067d30984348e4f7d3
jeromysmith-cloudflare <jeromysmith@gmail.com>
2026-02-04 15:00:03 -0600

    Update resources.js

--- functions/api/resources.js
+++ functions/api/resources.js
@@ -1,10 +1,9 @@
 export async function onRequest({ env }) {
   const data = await env.RESOURCES_KV.get("resources");
-
-  return new Response(data, {
+  return new Response(data ?? "[]", {
     headers: {
       "Content-Type": "application/json",
-      "Cache-Control": "public, max-age=300" // optional
+      "Cache-Control": "no-store"
     }
   });
 }
