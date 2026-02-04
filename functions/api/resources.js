a0b72811548cdf7a358d559203b41caaa1f2d4ad
jeromysmith-cloudflare <jeromysmith@gmail.com>
2026-02-04 12:45:10 -0600

    Create resources.js

---
+++ functions/api/resources.js
@@ -0,0 +1,10 @@
+export async function onRequest({ env }) {
+  const data = await env.RESOURCES_KV.get("resources");
+
+  return new Response(data, {
+    headers: {
+      "Content-Type": "application/json",
+      "Cache-Control": "public, max-age=300" // optional
+    }
+  });
+}
