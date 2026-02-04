b4e12cd5c82fc6a68ec750f14c47cd498bef2af7
jeromysmith-cloudflare <jeromysmith@gmail.com>
2026-02-04 15:53:25 -0600

    Update resources.js

--- functions/api/resources.js
+++ functions/api/resources.js
@@ -1,16 +1,5 @@
-export async function onRequest({ env, request }) {
-  return new Response(
-    JSON.stringify({
-      debug: "HIT-PAGES-FUNCTION",
-      time: new Date().toISOString(),
-      url: request.url
-    }),
-    {
-      headers: {
-        "Content-Type": "application/json",
-        "Cache-Control": "no-store",
-        "X-Debug": "HIT-PAGES-FUNCTION"
-      }
-    }
-  );
+export async function onRequest() {
+  return new Response("FUNCTION HIT OK", {
+    headers: { "Cache-Control": "no-store" }
+  });
 }
